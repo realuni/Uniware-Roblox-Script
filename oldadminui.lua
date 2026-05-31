364c0c0748fbc07ab1a860bf5ea1078746927255
realuni <contact.realuni@gmail.com>
2026-05-31 13:50:12 +0200

    Update oldadminui.lua

--- oldadminui.lua
+++ oldadminui.lua
@@ -88,7 +88,7 @@ local HintFollow = {
 --// This keeps the command panel visually locked to the same place on every monitor/window size.
 local MainLayout = {
 	RightOffset = 24,
-	BottomOffset = -10,
+	BottomOffset = -15,
 	ClosedPeek = 34,
 	Width = 285,
 	Height = 315,
